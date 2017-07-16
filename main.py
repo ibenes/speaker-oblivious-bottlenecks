70ec880c10591da1a86faa46a6573abe3bd4a829
Karel Benes <ibenes@fit.vutbr.cz>
2017-07-16 22:33:21 +0200

    main: draw samples, show them

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import torch
+
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     print('hello')
+
+    X = np.random.multivariate_normal([4,1], [[2, 0.5],[0.5, 0.5]], size=100)
+
+    plt.figure()
+    plt.scatter(X[:,0], X[:,1]) 
+    plt.show()
