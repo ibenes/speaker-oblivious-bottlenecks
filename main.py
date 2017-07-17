9e7b375b2fa1576026c8761a8965af7dc4c95c8f
Karel Benes <ibenes@fit.vutbr.cz>
2017-07-17 11:03:47 +0200

    main.py: add PhnSpkGenerator class

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ import torch
 
 import matplotlib.pyplot as plt
 
+class PhnSpkGenerator():
+    def __init__(self, mu, cov, phn, spk):
+        self._mu = mu
+        self._cov = cov
+        self._phn = phn
+        self._spk = spk
+
+    def generate(self, N):
+        X = np.random.multivariate_normal(self._mu, self._cov, size=N)
+        phn = torch.ones(N) * self._phn
+        spk = torch.ones(N) * self._spk
+        return torch.from_numpy(X).float(), phn, spk
+
 if __name__ == '__main__':
     print('hello')
 
-    X = np.random.multivariate_normal([4,1], [[2, 0.5],[0.5, 0.5]], size=100)
+    gen1 = PhnSpkGenerator([4,1], [[2, 0.5],[0.5, 0.5]], phn=0, spk=0)
+    X, phn, spk = gen1.generate(100)
 
     plt.figure()
-    plt.scatter(X[:,0], X[:,1]) 
+    plt.scatter(X.numpy()[:,0], X.numpy()[:,1]) 
     plt.show()
