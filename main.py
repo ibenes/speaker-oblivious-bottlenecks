6d534469c0cef4e1034b85483bbbe07b126c1440
Karel Benes <ibenes@fit.vutbr.cz>
2017-07-17 11:14:47 +0200

    main.py: instatiate multiple generators, collect all their outputs

--- main.py
+++ main.py
@@ -21,8 +21,32 @@ class PhnSpkGenerator():
 if __name__ == '__main__':
     print('hello')
 
+    phn_mus = []
+    phn_mus.append(np.asarray([1,1]))
+    phn_mus.append(np.asarray([3,-2]))
+    phn_mus.append(np.asarray([6,4]))
+
+    phn_covs = []
+    phn_covs.append(np.asarray([[1,0], [0,1]]))
+    phn_covs.append(np.asarray([[1,0], [0,1]]))
+    phn_covs.append(np.asarray([[1,0], [0,1]]))
+
     gen1 = PhnSpkGenerator([4,1], [[2, 0.5],[0.5, 0.5]], phn=0, spk=0)
     X, phn, spk = gen1.generate(100)
+
+    gens = []
+    for phn_mu, phn_cov in zip(phn_mus, phn_covs):
+        gens.append(PhnSpkGenerator(phn_mu, phn_cov, 0, 0))
+        
+    X = torch.zeros((0, 2))
+    t_phn = torch.zeros((0,))
+    t_spk = torch.zeros((0,))
+
+    for g in gens:
+        X_g, phn_g, spk_g = g.generate(100)
+        X = torch.cat([X, X_g], 0)
+        t_phn = torch.cat([t_phn, phn_g], 0)
+        t_spk = torch.cat([t_spk, spk_g], 0)
 
     plt.figure()
     plt.scatter(X.numpy()[:,0], X.numpy()[:,1]) 
