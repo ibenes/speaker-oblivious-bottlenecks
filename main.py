1ddedc3698669faac7bf3b5a271167e85098d3fe
Karel Benes <ibenes@fit.vutbr.cz>
2017-07-19 21:30:34 +0200

    dual_target_epoch(): rewrite for generic, N targets, case

--- main.py
+++ main.py
@@ -90,61 +90,58 @@ def epoch(fwd, params, X, target, batch_size=16, shuffle=True):
     return total_loss/nb_batches, correct/total
         
 
-def dual_target_epoch(common, dec1, dec2, params, X, t1, t2, batch_size=16, shuffle=True):
+def dual_target_epoch(common, decoders, params, X, targets, batch_size=16, shuffle=True):
+    assert len(decoders) == len(targets)
     N = X.size()[0]
-    assert t1.size()[0] == N
-    assert t2.size()[0] == N
+    for t in targets:
+        assert t.size()[0] == N
 
+    # TODO: clone?
     train_X = X
-    train_t1 = t1
-    train_t2 = t2
+    train_targets = targets
     if shuffle:
         p = np.random.permutation(N)
         train_X = X.numpy()[p]
-        train_t1 = t1.numpy()[p]
-        train_t2 = t2.numpy()[p]
+        train_X = torch.from_numpy(train_X)
+        for i, t in enumerate(train_targets):
+            train_targets[i] = torch.from_numpy(t.numpy()[p])
 
-    train_X = torch.from_numpy(train_X)
-    train_t1 = torch.from_numpy(train_t1)
-    train_t2 = torch.from_numpy(train_t2)
 
     nb_batches = N // batch_size
     total = nb_batches * batch_size
-    total_loss1 = 0.0
-    total_loss2 = 0.0
-    correct1 = 0
-    correct2 = 0
+    cum_losses = []
+    nb_correct = []
+    for i in range(len(decoders)):
+        cum_losses.append(0.0)
+        nb_correct.append(0)
 
     criterion = torch.nn.NLLLoss()
     optim = torch.optim.SGD(params, lr=1e-3)
 
     for i in range(nb_batches):
         batch_X = Variable(train_X[i*batch_size:(i+1)*batch_size])
-        batch_t1 = Variable(train_t1[i*batch_size:(i+1)*batch_size])
-        batch_t2 = Variable(train_t2[i*batch_size:(i+1)*batch_size])
+        batch_ts = []
+        for t in train_targets:
+            batch_ts.append(Variable(t[i*batch_size:(i+1)*batch_size]))
 
         repre = common(batch_X) 
 
-        y1 = dec1(repre)
-        loss1 = criterion(y1, batch_t1) 
-        _, preds1 = y1.max(dim=1)
-        correct1 += (preds1 == batch_t1).sum().data[0]
-        total_loss1 += loss1.data[0] 
+        losses = []
+        for i, (dec, t) in enumerate(zip(decoders, batch_ts)):
+            y = dec(repre)
+            loss = criterion(y, t) 
+            losses.append(loss)
+            _, preds = y.max(dim=1)
+            nb_correct[i] += (preds == t).sum().data[0]
+            cum_losses[i] += loss.data[0]
 
-        y2 = dec2(repre)
-        loss2 = criterion(y2, batch_t2) 
-        _, preds2 = y2.max(dim=1)
-        correct2 += (preds2 == batch_t2).sum().data[0]
-        total_loss2 += loss2.data[0] 
-
-        complete_loss = loss1 + loss2
+        complete_loss = sum(losses)
 
         optim.zero_grad()
         complete_loss.backward()
         optim.step()
 
-    return total_loss1/nb_batches, correct1/total, total_loss2/nb_batches, correct2/total
-
+    return cum_losses[0]/nb_batches, nb_correct[0]/total, cum_losses[1]/nb_batches, nb_correct[1]/total
 
 def instantiate_generators():
     phn_mus = []
@@ -169,14 +166,7 @@ def instantiate_generators():
 
     return gens
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--nb-epochs", type=int, default=200,
-                        help="number of training epochs")
-    args = parser.parse_args()
-
-    gens = instantiate_generators() 
-        
+def generate(gens, N_per_cluster):
     X = torch.zeros((0, 2))
     t_phn = torch.zeros((0,))
     t_spk = torch.zeros((0,))
@@ -188,6 +178,19 @@ if __name__ == '__main__':
         t_spk = torch.cat([t_spk, spk_g], 0)
     t_phn = t_phn.long()
     t_spk = t_spk.long()
+
+    return X, t_phn, t_spk
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nb-epochs", type=int, default=200,
+                        help="number of training epochs")
+    args = parser.parse_args()
+
+    gens = instantiate_generators() 
+        
+    X, t_phn, t_spk = generate(gens, 100)
 
     plotter = Plotter()
     #plotter.plot(X, t_phn, t_spk, name="Raw data")
@@ -244,9 +247,9 @@ if __name__ == '__main__':
     print("Training jointly, from same init:")
     for i in range(args.nb_epochs):
         phn_ce, phn_acc, spk_ce, spk_acc = dual_target_epoch(
-            bn_backup, phn_backup, spk_backup,
+            bn_backup, [phn_backup, spk_backup],
             chain(bn_backup.parameters(), phn_backup.parameters(), spk_backup.parameters()),
-            X, t_phn, t_spk
+            X, [t_phn, t_spk]
         )
         if i % 25 == 24:
             string = "{:>3} phn CE: {:.3f}, phn Acc: {:2.2f}, spk CE: {:.3f}, spk Acc: {:2.2f}".format(
