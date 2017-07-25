b91eac81e121b713ec4762adf11f1262f83715d7
Benes Karel <ibenes@fit.vutbr.cz>
2017-07-25 15:17:07 +0200

    instantiate_generators(): make the data more challenging

--- main.py
+++ main.py
@@ -129,14 +129,14 @@ def multi_target_epoch(common, decoders, optim, X, targets, batch_size=16, shuff
 
 def instantiate_generators():
     phn_mus = []
-    phn_mus.append(np.asarray([1,1]))
+    phn_mus.append(np.asarray([0.7,1.7]))
     phn_mus.append(np.asarray([3.5,2]))
-    phn_mus.append(np.asarray([2,4]))
+    phn_mus.append(np.asarray([2,4.2]))
 
     phn_covs = []
-    phn_covs.append(np.asarray([[0.2,0], [0,0.2]]))
-    phn_covs.append(np.asarray([[0.2,0], [0,0.2]]))
-    phn_covs.append(np.asarray([[0.2,0], [0,0.2]]))
+    phn_covs.append(np.asarray([[0.5,0], [0,0.2]]))
+    phn_covs.append(np.asarray([[0.2,-0.2], [-0.2,0.5]]))
+    phn_covs.append(np.asarray([[0.2,-0.15], [-0.15,0.2]]))
 
     spk_mus = []
     spk_mus.append(np.asarray([-0.7, 0.7]))
